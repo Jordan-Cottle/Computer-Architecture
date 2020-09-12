364a5640bc22e400794de917b8c21483593740ed
Jordan Cottle <jordancottle622@gmail.com>
2020-09-11 21:28:46 -0400

    Use random number for time

--- Assignment1/cpp_source/main.cpp
+++ Assignment1/cpp_source/main.cpp
@@ -2,18 +2,15 @@
 #include "event.h"
 
 #include <iostream>
-#include <ctime>
 #include <stdlib.h>
 
 int main()
 {
     EventQueue masterEventQueue;
 
-    for (int i = 0; i < 20; i++)
+    for (int i = 1; i <= 20; i++)
     {
-        time_t now = time(0);
-        tm *time = localtime(&now);
-        Event *event = new Event(i, rand() % 10 + 1);
+        Event *event = new Event(rand() % i, rand() % 10 + 1);
 
         masterEventQueue.push(event);
     }
