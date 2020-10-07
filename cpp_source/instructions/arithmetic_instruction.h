e31982040e0623277f6bf1a3f93ec488ce8075cf
Jordan Cottle <jordancottle622@gmail.com>
2020-10-07 18:54:31 -0400

    Split arithmetic instruction into header and cpp

--- cpp_source/instructions/arithmetic_instruction.h
+++ cpp_source/instructions/arithmetic_instruction.h
@@ -17,81 +17,21 @@ struct ArithmeticInstruction : DecodedInstruction
     bool immediate;
     int immediateValue;
 
-    ArithmeticInstruction(Instruction *instruction) : DecodedInstruction(instruction)
-    {
-        this->immediate = false;
+    ArithmeticInstruction(Instruction *instruction);
 
-        this->destinationIndex = instruction->arguments[0];
-        this->leftIndex = instruction->arguments[1];
-        this->rightIndex = instruction->arguments[2];
-    }
-
-    ArithmeticInstruction(Instruction *instruction, int immediateValue) : DecodedInstruction(instruction)
-    {
-        this->immediate = true;
-
-        this->destinationIndex = instruction->arguments[0];
-        this->leftIndex = instruction->arguments[1];
-
-        this->immediateValue = immediateValue;
-    }
+    ArithmeticInstruction(Instruction *instruction, int immediateValue);
 
     virtual void execute(Cpu *cpu) = 0;
 };
 
 struct Add : ArithmeticInstruction
 {
-    Add(Instruction *instruction) : ArithmeticInstruction(instruction)
-    {
-    }
-    Add(Instruction *instruction, int immediateValue) : ArithmeticInstruction(instruction, immediateValue)
-    {
-    }
+    Add(Instruction *instruction);
+    Add(Instruction *instruction, int immediateValue);
 
-    void execute(Cpu *cpu)
-    {
-        if (this->isFp)
-        {
-            double left = cpu->fpRegister.read(this->leftIndex);
+    void execute(Cpu *cpu);
 
-            double right = cpu->fpRegister.read(this->rightIndex);
-
-            cpu->fpRegister.write(this->destinationIndex, right + left);
-        }
-        else
-        {
-            int left = cpu->fpRegister.read(this->leftIndex);
-
-            int right = this->immediate ? this->immediateValue : cpu->intRegister.read(this->rightIndex);
-
-            cpu->fpRegister.write(this->destinationIndex, right + left);
-        }
-    }
-
-    std::string __str__()
-    {
-        std::string prefix;
-        if (this->operation[0] == 'f')
-        {
-            prefix = "F";
-        }
-        else
-        {
-            prefix = "R";
-        }
-
-        std::string s = prefix + str(this->destinationIndex) + " <- " + prefix + str(this->leftIndex) + " + ";
-        if (this->immediate)
-        {
-            s += "#" + str(this->immediateValue);
-        }
-        else
-        {
-            s += prefix + str(this->rightIndex);
-        }
-
-        return s;
-    }
+    std::string __str__();
 };
 
 #endif
