7b5600ddfeee369c1eb0359d59fb3c1f53e1e75d
Jordan Cottle <jordancottle622@gmail.com>
2020-11-18 23:43:13 -0500

    Add a memory router class to split data and instruction requests

---
+++ cpp_source/memory/memory_router.cpp
@@ -0,0 +1,63 @@
+/*
+    Author: Jordan Cottle
+    Created: 11/18/2020
+*/
+
+#include "memory_router.h"
+
+MemoryRouter::MemoryRouter(MemoryInterface *instrutionCache, MemoryInterface *dataCache) : MemoryInterface(0, 0)
+{
+    this->memoryMap = std::unordered_map<uint32_t, MemoryInterface *>();
+}
+
+MemoryInterface *MemoryRouter::selectMemoryDevice(SimulationDevice *device)
+{
+    if (device->type == "Fetch")
+    {
+        return this->instrutionCache;
+    }
+
+    return this->dataCache;
+}
+
+MemoryInterface *MemoryRouter::selectMemoryDevice(uint32_t address)
+{
+    return this->memoryMap[address];
+}
+
+bool MemoryRouter::request(uint32_t address, SimulationDevice *device)
+{
+    MemoryInterface *memoryDevice = this->selectMemoryDevice(device);
+    this->memoryMap[address] = memoryDevice;
+    return memoryDevice->request(address, device);
+}
+
+uint32_t MemoryRouter::readUint(uint32_t address)
+{
+    return this->selectMemoryDevice(address)->readUint(address);
+}
+
+int MemoryRouter::readInt(uint32_t address)
+{
+    return this->selectMemoryDevice(address)->readInt(address);
+}
+
+float MemoryRouter::readFloat(uint32_t address)
+{
+    return this->selectMemoryDevice(address)->readFloat(address);
+}
+
+void MemoryRouter::write(uint32_t address, uint32_t value)
+{
+    this->selectMemoryDevice(address)->write(address, value);
+}
+
+void MemoryRouter::write(uint32_t address, int value)
+{
+    this->selectMemoryDevice(address)->write(address, value);
+}
+
+void MemoryRouter::write(uint32_t address, float value)
+{
+    this->selectMemoryDevice(address)->write(address, value);
+}
